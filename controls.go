e78e9e6d0eef536288d237975d5845293048f031
Christopher Puschmann <christopher.puschmann@dbschenker.com>
2021-08-09 23:49:58 +0200

    Update IberMessage signature

--- controls.go
+++ controls.go
@@ -4,8 +4,10 @@ import ber "github.com/go-asn1-ber/asn1-ber"
 
 var _ IBerMessage = (*Controls)(nil)
 
-type Control IBerMessage
-type Controls []Control
+type (
+	Control  IBerMessage
+	Controls []Control
+)
 
 func (c Controls) Marshal() (_ *ber.Packet, controls *ber.Packet) {
 	packet := ber.Encode(ber.ClassContext, ber.TypeConstructed, 0, nil, "Controls")
